f1fd569aaee0c88dfd61536987e83f4184dbf17e
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-05-13 14:47:10 +0530

    greedy completed

--- dynamicProgramming2/lectures/subsetSum.cpp
+++ dynamicProgramming2/lectures/subsetSum.cpp
@@ -47,6 +47,39 @@ bool subsetSum(int* input, int n, int k){
     return ans;
 }
 
+bool subsetSum_memOptmized(int* val,int n,int sum){
+
+	bool** dp = new bool*[2];
+	for(int i=0;i<=1;i++){
+		dp[i] = new bool[sum+1];
+	}
+
+	for(int i=0;i<=sum;i++){
+		dp[0][i] = false;
+	}
+	dp[0][0] = true;
+
+	int flag = 1;
+
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=sum;j++){
+
+			dp[flag][j] = dp[flag^1][j];
+
+			if(j>=val[i-1]){
+				dp[flag][j] = dp[flag][j] || dp[flag^1][j-val[i-1]];
+			}
+		}
+		flag = flag ^ 1;
+	}
+	bool ans = dp[flag^1][sum];
+	// delete dp array
+    delete [] dp[0];
+    delete [] dp[1];
+    delete [] dp;
+	return ans;
+}
+
 int main()
 {
     int n,k;
