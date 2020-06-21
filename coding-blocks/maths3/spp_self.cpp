3ee17ff944c80c92d8cffb903d424dc9ee0ff6e8
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-06-21 20:47:47 +0530

    added info

--- coding-blocks/maths3/spp_self.cpp
+++ coding-blocks/maths3/spp_self.cpp
@@ -1,3 +1,35 @@
+/*
+Sequence (ai) of natural numbers is defined as follows:
+
+ai = bi (for i <= k)
+ai = c1(a(i-1)) + c2(a(i-2)) + ... + ck(a(i-k)) (for i > k)
+
+where bj and cj are given natural numbers for 1<=j<=k. 
+Your task is to compute am + am+1 + am+2 + ... + an for given m <= n and 
+output it modulo a given positive integer p.
+
+Input
+On the first row there is the number C of test cases (equal to about 50).
+Each test contains four lines:
+k - number of elements of (c) and (b) (1 <= k <= 15)
+b1,...,bk - k natural numbers where 0 <= bj <= 109 separated by spaces
+c1,...,ck - k natural numbers where 0 <= cj <= 109 separated by spaces
+m, n, p - natural numbers separated by spaces (1 <= m <= n <= 1018, 1<= p <= 108)
+Output
+Exactly C lines, one for each test case: (am + am+1 + am+2 + ... + an) modulo p.
+
+Example
+Input:
+1
+2
+1 1
+1 1
+2 10 1000003
+
+Output:
+142
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
---
+++ coding-blocks/maths3/staircase_problem.cpp
@@ -0,0 +1,14 @@
+/*
+you are given n and k 
+you have to find number of ways to reach nth step 
+when you have following options
+you can take 1 step at a time
+you can take 2 step at a time
+you can take 3 step at a time
+|
+|
+|
+you can take k step at a time
+
+use matrix exponentiation to solve this problem
+*/
