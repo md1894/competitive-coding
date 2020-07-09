fc84a596407acc6577fd7e9acb79f82d0af25081
Mehul Dubey <mehul.narendra.dubey@gmail.com>
2020-07-09 12:20:52 +0530

    created FAST_FIBONACCI

---
+++ coding-blocks/maths-assignments/FAST_FIBONACCI.cpp
@@ -0,0 +1,37 @@
+/*
+
+
+Fibonacci series is well-known series in which in which each number (Fibonacci number) is the sum of the two preceding numbers. 
+The series looks like 1, 1, 2, 3, 5, 8…. and so on. Your task is to find nth number.
+Since the number can be large, output the answer modulo (109+7).
+Input Format
+
+An integer T, denoting the number of test cases. Each test case contains and integer N.
+Constraints
+
+1<=T<=10^5
+1<=N<=10^9
+Output Format
+
+Print the nth Fibonacci number modulo (109+7).
+
+Sample Input
+
+4
+3
+4
+5
+6
+
+Sample Output
+
+2
+3
+5
+8
+
+Explanation
+
+These terms are given in problem description.
+
+*/
