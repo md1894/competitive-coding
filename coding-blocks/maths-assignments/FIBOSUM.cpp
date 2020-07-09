40b19a83834649393fc3d45947ec708282a3097d
Mehul Dubey <mehul.narendra.dubey@gmail.com>
2020-07-09 12:24:23 +0530

    Create FIBOSUM

---
+++ coding-blocks/maths-assignments/FIBOSUM.cpp
@@ -0,0 +1,36 @@
+/*
+
+
+The Fibonacci sequence is defined by the following relation:
+F(0) = 0
+F(1) = 1
+F(N) = F(N - 1) + F(N - 2), N >= 2
+Your task is very simple. 
+Given two non-negative integers N and M, 
+you have to calculate the sum (F(N) + F(N + 1) + … + F(M)) mod 1000000007.
+Source: FIBOSUM (SPOJ)
+
+Input Format
+
+The first line contains an integer T (the number of test cases). 
+Then, T lines follow. Each test case consists of a single line with two non-negative integers N and M.
+Constraints
+
+1<=T<=1000 
+0<=N<=M<=10^9
+
+Output Format
+For each test case you have to output a single line containing the answer for the task.
+
+Sample Input
+3
+0 3
+3 5
+10 19
+
+Sample Output
+4
+10
+10857
+
+*/
