a3e2e00c7dfeaf180b8bfadf21fc3d5779d23048
mehulkumar18021994 <dubey.mehulkumar@gmail.com>
2020-07-10 08:18:57 +0530

    changes in format of question

---
+++ coding-blocks/maths-assignments/diceRoll.cpp
@@ -0,0 +1,25 @@
+/*
+In a game of fair dice(6 sides) throw, 
+what is the expected number of throws to make sure that all 6 outcomes appear atleast once? 
+Round off the ans to next integer.
+
+Input Format
+There will be no input.
+
+Constraints
+No Constraints
+
+Output Format
+The output will be an integer.
+
+Sample Input
+There is no sample input.
+Sample Output
+There is no sample output.
+*/
+
+#include<iostream>
+int main() {
+	std::cout << 15;
+	return 0;
+}
--- coding-blocks/maths-assignments/interviewCandidates.cpp
+++ coding-blocks/maths-assignments/interviewCandidates.cpp
@@ -1,5 +1,7 @@
 /*
-Candidates are appearing for interview one after other. Probability of each candidate getting selected is 0.16. What is the expected number of candidates that you will need to interview to make sure that you select somebody?
+Candidates are appearing for interview one after other. 
+Probability of each candidate getting selected is 0.16. What is the expected number of 
+candidates that you will need to interview to make sure that you select somebody?
 */
 
 #include<iostream>
