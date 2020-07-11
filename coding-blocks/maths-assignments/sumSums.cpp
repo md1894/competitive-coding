3a720589ef0052fb474befdb4007c5a235aef232
Mehul Dubey <mehul.narendra.dubey@gmail.com>
2020-07-11 13:03:14 +0530

    Structures sumsums problem

--- coding-blocks/maths-assignments/sumSums.cpp
+++ coding-blocks/maths-assignments/sumSums.cpp
@@ -1,7 +1,12 @@
 /*
-The N (1 ≤ N ≤ 50,000) cows, conveniently numbered 1, 2, …, N, are trying to learn some encryption algorithms. After studying a few examples, they have decided to make one of their own! However, they are not very experienced at this, so their algorithm is very simple:
+The N (1 ≤ N ≤ 50,000) cows, conveniently numbered 1, 2, …, N, 
+are trying to learn some encryption algorithms.
+After studying a few examples, they have decided to make one of their own! 
+However, they are not very experienced at this, 
+so their algorithm is very simple:
 
-Each cow i is given a starting number Ci (0 ≤ Ci < 90,000,000), and then all the cows perform the following process in parallel:
+Each cow i is given a starting number Ci (0 ≤ Ci < 90,000,000), 
+and then all the cows perform the following process in parallel:
 
 First, each cow finds the sum of the numbers of the other N-1 cows. 
 After all cows are finished, each cow replaces her number with the sum she computed. 
