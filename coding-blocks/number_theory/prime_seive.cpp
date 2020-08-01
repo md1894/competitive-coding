3c0292c1d3e5396e3b831f45c2e42e6ed939e1d7
md1894 <mehul.narendra.dubey@gmail.com>
2020-08-01 23:35:44 +0530

    added prime seive concept

--- coding-blocks/number_theory/prime_seive.cpp
+++ coding-blocks/number_theory/prime_seive.cpp
@@ -1,27 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-void prime_seive(int* a){
+void prime_seive(int *a)
+{
     //skip all even numbers
-    for(int i = 3; i <= 100000; i += 2){
+    for (int i = 3; i <= 100000; i += 2)
+    {
         a[i] = 1;
     }
-    for(int i = 3; i*i <= 100000; i += 2){
-        if(a[i] == 1){
-            for(int j = i*i; j <= 100000; j += i){
+    for (int i = 3; i * i <= 100000; i += 2)
+    {
+        if (a[i] == 1)
+        {
+            for (int j = i * i; j <= 100000; j += i)
+            {
                 a[j] = 0;
             }
         }
     }
 }
 
-int main(){
+int main()
+{
     int a[100001] = {0};
     prime_seive(a);
     int n;
     cin >> n;
-    for(int i = 3; i <= n; i++){
-        if(a[i] == 1)
+    for (int i = 3; i <= n; i++)
+    {
+        if (a[i] == 1)
             cout << i << " ";
     }
     cout << endl;
--- coding-blocks/number_theory/prime_visits.cpp
+++ coding-blocks/number_theory/prime_visits.cpp
@@ -29,4 +29,67 @@ For the second testcase , he chooses countries with numbers 11,13,17 and 19.
 https://hack.codingblocks.com/app/practice/3/1039/problem
 
 */
+#include <bits/stdc++.h>
+#define N 1000001
+using namespace std;
 
+typedef long long int ll;
+
+//loop condition
+/*
+suppose you have to check weather 36 is prime or not
+possible divisors of 36 is 
+1, 2, 3, 4,    '6' (sqrt(N))   , 9, 12, 18, 36
+for each divisor of 36 greater tha sqrt(36) there is a corresponding divisor greater than sqrt(36)
+so rather than checking weather there is any divisor in the range 1 to 36, (i.e. 1 to N)
+we will check in the range 1 to 6, (i.e. sqrt(N))
+*/
+
+void prime_seive(int *p)
+{
+    //special case
+    p[2] = 1;
+    p[0] = p[1] = 0;
+    //mark every odd number as a prime number
+    //even number cannot be a prime number
+    for (int i = 3; i < N; i += 2)
+    {
+        p[i] = 1;
+    }
+    // for the condition of this for loop please refer the above condition
+    for (int i = 3; i * i < N; i += 2)
+    {
+        // if the number is a prime number
+        // mark all of its multiples as not prime number
+        if (p[i] == 1)
+        {
+            for (ll j = i * i; j < N; j += i)
+            {
+                p[j] = 0;
+            }
+        }
+    }
+    // at this stage we get the array whose index denotes weather that number is
+    // prime or not
+}
+
+int main()
+{
+    ll q, a, b;
+    cin >> q;
+    int p[N] = {0};
+    int csum[N] = {0};
+    prime_seive(p);
+    // compute the cummulative sum of primes upto N
+    for (int i = 1; i < N; i++)
+    {
+        csum[i] = csum[i - 1] + p[i];
+    }
+    // answer all query in O(1) time
+    while (q--)
+    {
+        cin >> a >> b;
+        cout << csum[b] - csum[a - 1] << endl;
+    }
+    return 0;
+}
