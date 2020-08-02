23ab4ae27ddb424bf72bfbcf370ad154b3cc261e
md1894 <mehul.narendra.dubey@gmail.com>
2020-08-03 00:12:59 +0530

    seive problem added

--- coding-blocks/number_theory/segmented_seive.cpp
+++ coding-blocks/number_theory/segmented_seive.cpp
@@ -32,9 +32,64 @@ Warning: large Input/Output data, be careful with certain languages
 */
 
 #include <bits/stdc++.h>
+#define N 100000
 using namespace std;
+typedef long long int ll;
+int p[N + 1] = {0};
+vector<int> primes;
+
+void seive()
+{
+    for (int i = 2; i < N; i++)
+    {
+        if (p[i] == 0)
+        {
+            primes.push_back(i);
+            for (int j = 2 * i; j <= N; j += i)
+            {
+                p[j] == 1;
+            }
+        }
+    }
+}
 
 int main()
 {
+    seive();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll m, n;
+        cin >> m >> n;
+        bool segment[n - m + 1];
+        for (int i = 0; i < (n - m + 1); i++)
+        {
+            segment[i] = 0;
+        }
+        for (int i = 0; i < primes.size(); i++)
+        {
+            if ((primes[i] * primes[i]) > n)
+                break;
+
+            int start = (m / primes[i]) * primes[i];
+            if (primes[i] >= m && primes[i] <= n)
+            {
+                start = primes[i] * 2;
+            }
+            for (int j = start; j <= n; j += primes[i])
+            {
+                segment[j - m] = 1;
+            }
+        }
+        for (int i = m; i <= n; i++)
+        {
+            if (segment[i - m] == 0 && i != 1)
+            {
+                cout << i << endl;
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
