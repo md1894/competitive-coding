8b790c8d20bb8090d29ba4888415c1f7d6e9a618
Mehul Dubey <mehul.narendra.dubey@gmail.com>
2020-08-21 10:32:27 +0530

    added explanations to the extended euclid algo

--- coding-blocks/euclid_algo/extended_euclid.cpp
+++ coding-blocks/euclid_algo/extended_euclid.cpp
@@ -1,3 +1,24 @@
+/*
+
+Extended Euclidean Algorithm:
+Extended Euclidean algorithm also finds integer coefficients x and y such that:
+
+ax + by = gcd(a, b) 
+
+Examples:
+
+Input: a = 30, b = 20
+Output: gcd = 10
+        x = 1, y = -1
+(Note that 30*1 + 20*(-1) = 10)
+
+Input: a = 35, b = 15
+Output: gcd = 5
+        x = 1, y = -2
+(Note that 35*1 + 15*(-2) = 5)
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
