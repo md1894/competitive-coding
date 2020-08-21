05ec8e02f8649cf372965cd202a3117b7c312c1f
kimaya <kimaya@localhost.localdomain>
2020-08-21 10:20:53 +0530

    numTheo

---
+++ coding-blocks/numberTheory_Theorems/largeNumExpo.cpp
@@ -0,0 +1,13 @@
+/*
+problem statement : Given A and B, we have to find (A power B) % (10^9 + 7) where A, B<=10^100000
+
+Fast Modulo Exponentiation, Modulo Properties, Fermat’s Little Theorem
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
